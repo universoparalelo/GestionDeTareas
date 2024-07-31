79788294335f52d868d8aad5d5d794b081de0e7e
UniversoParalelo <cmartinrodich@gmail.com>
2024-07-31 17:41:29 -0300

    primer commit

---
+++ clases.py
@@ -0,0 +1,262 @@
+'''
+Desafío 3: Sistema de Gestión de Tareas
+Objetivo: Desarrollar un sistema para organizar y administrar tareas personales o de equipo.
+
+Requisitos:
+
+Crear una clase base Tarea con atributos como descripción, fecha de vencimiento, estado (pendiente, en progreso, completada), etc.
+Definir al menos 2 clases derivadas para diferentes tipos de tareas (por ejemplo, TareaSimple, TareaRecurrente) con atributos y métodos específicos.
+Implementar operaciones CRUD para gestionar las tareas.
+Manejar errores con bloques try-except para validar entradas y gestionar excepciones.
+Persistir los datos en archivo JSON.
+##############################'''
+
+from datetime import (date, timedelta, datetime)
+import json
+
+class Tarea:
+    def __init__(self, titulo, descripcion, f_vencimiento, estado):
+        self.__titulo = self.validarTitulo(titulo)
+        self.__descripcion = self.validardescripcion(descripcion)
+        self.__f_vencimiento = self.validarf_vencimiento(f_vencimiento)
+        self.__estado = self.validarEstado(estado)
+    
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @property
+    def descripcion(self):
+        return self.__descripcion
+    
+    @property
+    def f_vencimiento(self):
+        return self.__f_vencimiento
+    
+    @property
+    def estado(self):
+        return self.__estado
+    
+    @titulo.setter
+    def titulo(self, nuevoTitulo):
+        self.__titulo = self.validarTitulo(nuevoTitulo)
+    
+    def validarTitulo(self, nuevoTitulo):
+        try:
+            if len(nuevoTitulo) > 30:
+                raise Exception("El titulo es muy largo.")
+            elif len(nuevoTitulo) == 0:
+                raise Exception("El titulo no puede estar vacio")
+            else:
+                return nuevoTitulo
+        except Exception as e:
+            raise Exception(f"{e}")
+        
+
+    @descripcion.setter
+    def descripcion(self, nuevaDescripcion):
+        self.__descripcion = self.validardescripcion(nuevaDescripcion)
+    
+    def validardescripcion(self, nuevaDescripcion):
+        try:
+            if len(nuevaDescripcion) > 255:
+                raise Exception('La descripcion es muy larga.')
+            else:
+                return nuevaDescripcion
+        except Exception as e:
+            raise Exception(f"{e}")
+            
+
+    @f_vencimiento.setter
+    def f_vencimiento(self, nuevaF_vencimiento):
+        self.__f_vencimiento = self.validarf_vencimiento(nuevaF_vencimiento)
+    
+    def validarf_vencimiento(self, nuevaF_vencimiento):
+        try:
+            if nuevaF_vencimiento == "":
+                raise Exception("La tarea debe tener una fecha de vencimiento")
+            else:
+                nuevaF_vencimiento = date.fromisoformat(nuevaF_vencimiento)
+                if nuevaF_vencimiento < date.today():
+                    raise Exception("La fecha no es válida. Debe ser mayor a la fecha actual")
+                else:
+                    return str(nuevaF_vencimiento)
+        except ValueError as e:
+            raise Exception(f'El formato de fecha es incorrecto. {e}')
+        except Exception as e:
+            raise Exception(f"{e}")     
+            
+    @estado.setter
+    def estado(self, nuevoEstado):
+        self.__estado = self.validarEstado(nuevoEstado)
+
+    def validarEstado(self, nuevoEstado):
+        try:
+            if nuevoEstado not in ['pendiente', 'en progreso', 'completada']:
+                raise Exception("El estado no se corresponde con ninguna de las opciones válidas.")
+            else:
+                return nuevoEstado
+        except Exception as e: 
+            raise Exception(f"{e}")
+
+    def to_dict(self):
+        return {
+            "titulo": self.titulo,
+            "descripcion": self.descripcion,
+            "f_vencimiento": self.f_vencimiento,
+            "estado": self.estado,
+        }
+
+
+class TareaRecurrente(Tarea):
+    def __init__(self, titulo, descripcion, f_vencimiento, estado, recurrencia):
+        super().__init__(titulo, descripcion, f_vencimiento, estado)
+        self.__recurrencia = self.validarRecurrencia(recurrencia)
+
+    @property
+    def recurrencia(self):
+        return self.__recurrencia
+
+    @recurrencia.setter
+    def recurrencia(self, nuevoRecurrencia):
+        self.__recurrencia = self.validarRecurrencia(nuevoRecurrencia)
+
+    def validarRecurrencia(self, nuevoRecurrencia):
+        try: 
+            if nuevoRecurrencia == '':
+                raise Exception("El atributo recurrencia no puede estar vacio.")
+            elif int(nuevoRecurrencia) <= 0:
+                raise Exception("La recurrencia no puede ser un número negativo.")
+            else: 
+                return int(nuevoRecurrencia)
+        except Exception as e:
+            raise Exception(f"{e}")
+
+    def to_dict(self):
+        datos = super().to_dict()
+        datos['recurrencia'] = self.recurrencia
+        return datos
+
+
+class TareaSimple(Tarea):
+    def __init__(self, titulo, descripcion, f_vencimiento, estado, importancia):
+        super().__init__(titulo, descripcion, f_vencimiento, estado)
+        self.__importancia = self.validarimportancia(importancia)
+
+    @property
+    def importancia(self):
+        return self.__importancia
+    
+    @importancia.setter
+    def importancia(self, nuevaImportancia):
+        self.__importancia = self.validarimportancia(nuevaImportancia)
+
+    def validarimportancia(self, nuevaImportancia):
+        try:
+            if nuevaImportancia not in ['baja', 'media', 'alta']:
+                raise Exception("La importancia no se corresponde con ninguna de las opciones válidas.")
+            else:
+                return nuevaImportancia
+        except Exception as e:
+            raise Exception(f"{e}")
+
+
+    def to_dict(self):
+        datos = super().to_dict()
+        datos['importancia'] = self.importancia
+        return datos
+
+
+class Gestion():
+    def __init__(self, archivo):
+        self.archivo = archivo
+
+    def leer_datos(self):
+        try:
+            with open(self.archivo, 'r') as file:
+                datos = json.load(file)
+        except FileNotFoundError:
+            return {}
+        except Exception as error:
+            raise Exception(f'Error al leer datos del archivo: {error}')
+        else:
+            return datos
+
+    def guardar_datos(self, datos):
+        try:
+            with open(self.archivo, 'w') as file:
+                json.dump(datos, file, indent=4)
+        except IOError as error:
+            print(f'Error al intentar guardar los datos en {self.archivo}: {error}')
+        except Exception as error:
+            print(f'Error inesperado: {error}')
+
+    def crear_tarea(self, tarea):
+        try:
+            datos = self.leer_datos()
+            if tarea.titulo in datos.keys():
+                raise Exception(f"Ya existe una tarea con el mismo titulo '{tarea.titulo}'")
+            else:
+                datos[tarea.titulo] = tarea.to_dict()
+                self.guardar_datos(datos)
+                print(f"La tarea '{tarea.titulo}' se creó correctamente.")
+        except Exception as e:
+            raise Exception(f"{e}.")          
+
+    def leer_una_tarea(self, titulo):
+        try:
+            datos = self.leer_datos()
+
+            if titulo in datos.keys():
+                return datos[titulo]
+            else: 
+                raise Exception(f"No existe ninguna tarea con el titulo '{titulo}'")
+        except Exception as e:
+            raise Exception(f"Surgió un error en la lectura: {e}")
+
+    def actualizar_tarea(self, atributo, nuevo_valor, tarea):
+        try:
+            datos = self.leer_datos()
+
+            if tarea['titulo'] in datos.keys():
+                datos[tarea['titulo']][atributo] = nuevo_valor
+                self.guardar_datos(datos)
+                print(f"Tarea '{tarea['titulo']}' actualizada correctamente")
+            else:
+                print("No se encontró la tarea")
+        except Exception as e:
+            raise Exception(f"Ocurrió un error en la actualización: {e}")
+
+    def eliminar_tarea(self, tarea):
+        try:
+            datos = self.leer_datos()
+            
+            if tarea in datos.keys():
+                del datos[tarea]
+                self.guardar_datos(datos)
+                print(f"Tarea '{tarea}' eliminada correctamente")
+            else:
+                print(f"No se encontro la tarea '{tarea}'")
+        except Exception as e:
+            raise Exception(f"Ocurrió un error en la eliminacion: {e}")
+
+    def leer_segun_fecha(self, f_limite):
+        try:
+            datos = self.leer_datos()
+            if f_limite == '1':
+                days = 0
+            elif f_limite == '2':
+                days = 7
+            elif f_limite == '3':
+                days = 30
+            else:
+                print("Opción inválida.")
+            
+            nuevos_datos = {}
+            for tarea in datos:
+                if date.fromisoformat(datos[tarea]['f_vencimiento']) - date.today() <= timedelta(days=days):
+                    nuevos_datos[tarea] = datos[tarea]
+            
+            return nuevos_datos
+        except Exception as e:
+            raise Exception(f"{e}")
---
+++ main.py
@@ -0,0 +1,239 @@
+from clases import (TareaRecurrente, TareaSimple, Gestion)
+from datetime import(date, timedelta)
+import os
+import platform
+
+
+def limpiar_pantalla():
+    print(platform.system())
+    if platform.system() == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+def menu_para_mostrar():
+    print("1. Tareas del dia")
+    print("2. Tareas de la semana")
+    print("3. Tareas del mes")
+    print("4. Todas las tareas")
+    return input("Elija una opcion(1-4): ").strip()
+
+
+def menu_actualizacion(tarea):
+    print("========( Actualizacion )==========")
+    print("1. Titulo - 2. Descripcion - 3. Fecha de vencimiento - 4. Estado", end="")
+    if tarea.get('importancia'):
+        print(" - 5. Importancia")
+    else: 
+        print(" - 5. Recurrencia") 
+    return input("Elija una opción (1-5): ").strip()
+
+
+def cambiar_tarea(archivo, tarea, atributo):
+
+    try:
+        nuevo_atributo = input(f"Escriba el nuevo valor de '{atributo}': ").strip()
+        if atributo == 'titulo':
+            if nuevo_atributo == "":
+                raise Exception("El titulo no puede estar vacio")
+            elif len(nuevo_atributo) > 30:
+                raise Exception("El titulo es muy largo")
+            else:
+                archivo.actualizar_tarea(atributo, nuevo_atributo, tarea)
+        elif atributo == 'descripcion':
+            if len(nuevo_atributo) > 255:
+                raise Exception("La descripción es muy larga")
+            else:
+                archivo.actualizar_tarea(atributo, nuevo_atributo, tarea)
+        elif atributo == 'f_vencimiento':
+            if nuevo_atributo == "":
+                raise Exception("La fecha de vencimiento no puede estar vacio")
+            else:
+                nuevo_atributo = date.fromisoformat(nuevo_atributo)
+                if nuevo_atributo < date.today():
+                    raise Exception("La fecha no es válida. Debe ser mayor a la fecha actual")
+                else:
+                    archivo.actualizar_tarea(atributo, nuevo_atributo, tarea)
+        elif atributo == 'estado':
+            if nuevo_atributo == "":
+                raise Exception("El estado no puede estar vacio")
+            elif nuevo_atributo not in ["pendiente", "en progreso", "completada"]:
+                raise Exception("Valor inválido de estado.")
+            else:
+                archivo.actualizar_tarea(atributo, nuevo_atributo, tarea)
+        elif atributo == 'recurrencia':
+            if int(nuevo_atributo) <= 0:
+                raise Exception("La recurrencia no puede ser un número negativo o cero.")
+            else:
+                archivo.actualizar_tarea(atributo, nuevo_atributo, tarea)
+        elif atributo == 'importancia':
+            if nuevo_atributo == "":
+                raise Exception("La importancia no puede estar vacia")
+            elif nuevo_atributo not in ["alta", "media", "baja"]:
+                raise Exception("Valor inválido de importancia")
+            else:
+                archivo.actualizar_tarea(atributo, nuevo_atributo, tarea)
+    except ValueError:
+        print("El formato de fecha no es correcto.")
+    except Exception as e:
+        print(f"{e}")
+
+
+def menu():
+    print("========( Bienvenidx )=========")
+    print("1. Crear una tarea simple")
+    print("2. Crear una tarea recurrente")
+    print("3. Leer una tarea")
+    print("4. Actualizar una tarea")
+    print("5. Eliminar una tarea")
+    print("6. Ver todas las tareas")
+    print("7. Salir")
+    print("===========================")
+
+
+def crear_tarea(archivo, tipo_tarea):
+    try:
+        print("===========================")
+        print("Creando una tarea...")
+        titulo = input("Titulo: ").strip()
+        descripcion = input("Descripcion: ").strip()
+        f_vencimiento = input("Fecha de vencimiento(AAAA-MM-DD): ").strip()
+        estado = input("Estado(pendiente, en progreso, completada): ").strip()
+
+        if tipo_tarea == '1':
+            importancia = input("Importancia(baja, media, alta): ").strip()
+            tarea = TareaSimple(titulo, descripcion, f_vencimiento, estado, importancia) 
+        else:
+            recurrencia = input("Recurrencia en dias: ").strip()
+            tarea = TareaRecurrente(titulo, descripcion, f_vencimiento, estado, recurrencia)
+
+        archivo.crear_tarea(tarea)
+    except Exception as e:
+        print(f"{e}")
+        
+    print("===========================")
+    input("Presiona enter para continuar...") 
+
+
+def actualizar_tarea(archivo):
+    try:
+        tarea = archivo.leer_una_tarea(input("Escriba el titulo de la tarea a actualizar: ").strip())
+        opt = menu_actualizacion(tarea)
+
+        if opt == '1':
+            cambiar_tarea(archivo, tarea, 'titulo')
+        elif opt == '2':
+            cambiar_tarea(archivo, tarea, 'descripcion')
+        elif opt == '3':
+            cambiar_tarea(archivo, tarea, 'f_vencimiento')
+        elif opt == '4':
+            cambiar_tarea(archivo, tarea, 'estado')
+        elif opt == '5':
+            if tarea.get("importancia"):
+                cambiar_tarea(archivo, tarea, 'importancia')
+            else:
+                cambiar_tarea(archivo, tarea, 'recurrencia')
+        else:
+            print("Opcion inválida.")
+    except Exception as e:
+        print(f"{e}")
+
+    input("Presiona enter para continuar...") 
+
+
+def leer_una_tarea(archivo):
+    b_titulo = input("Escriba un titulo: ").strip()
+    tarea = archivo.leer_una_tarea(b_titulo)
+
+    if tarea:
+        print("===========================")
+        print(f"Tarea: {tarea['titulo']}")
+        print(f"Descripcion: {tarea['descripcion']}")
+        print(f"Fecha vencimiento: {tarea['f_vencimiento']}")
+        print(f"Estado: {tarea['estado']}")
+        if tarea.get('importancia'):
+            print(f"Importancia: {tarea['importancia']}")
+        else:
+            print(f"Recurrencia: {tarea['recurrencia']}")
+        print("===========================")
+        
+    else:
+        print(f"No se encontró ninguna tarea con el titulo '{b_titulo}'")
+
+    input("Presiona enter para continuar...") 
+
+
+def eliminar_tarea(archivo):
+    print("===========================")
+
+    titulo = input("Escriba el titulo de la tarea: ")
+    archivo.eliminar_tarea(titulo)
+
+    print("===========================")
+    input("Presiona enter para continuar...") 
+
+
+def mostrar_tareas(archivo):
+    opt = menu_para_mostrar()
+    if opt == '4':
+        datos = archivo.leer_datos()
+    else:
+        datos = archivo.leer_segun_fecha(opt)
+    cont = 1
+
+    print("=======( Mostrando tareas )======")
+    print("Tarea | Fecha de vencimiento | Estado | Importancia/Recurrencia")
+    for tarea in datos:
+        if datos[tarea].get('importancia'):
+            print(f"-------------({cont})-----------------")
+            print(f"{datos[tarea]['titulo']} | {datos[tarea]['f_vencimiento']} | {datos[tarea]['estado']} | {datos[tarea]['importancia']}")
+            cont += 1
+        else:
+            if opt == '1': days = 1
+            elif opt == '2': days = 7
+            elif opt == '3': days = 30
+            else: 
+                days = date.fromisoformat(datos[tarea]['f_vencimiento']) - date.today()
+                days = days.days
+
+            f_vencimiento = date.fromisoformat(datos[tarea]['f_vencimiento'])
+            f_limite = date.today() + timedelta(days=days)
+            cont_int = 1
+
+            while (f_vencimiento <= f_limite) and (cont_int != 5):
+                print(f"-------------({cont})-------------")
+                print(f"{datos[tarea]['titulo']} | {f_vencimiento} | {datos[tarea]['estado']} | {datos[tarea]['recurrencia']}")
+                cont += 1
+                cont_int += 1
+                f_vencimiento += timedelta(days=int(datos[tarea]['recurrencia']))
+    
+    print("=============================")
+    input("Presiona enter para continuar...") 
+
+
+if __name__ == '__main__':
+    archivo_tareas = 'laboratorioPOO/tareas_db.json'
+    gestion = Gestion(archivo_tareas)
+
+    while True:
+        limpiar_pantalla()
+        menu()
+        opcion = input("Elija una opcion: ").strip()
+
+        if opcion == '1' or opcion == '2':
+            crear_tarea(gestion, opcion)
+        elif opcion == '3':
+            leer_una_tarea(gestion)
+        elif opcion == '4':
+            actualizar_tarea(gestion)
+        elif opcion == '5': 
+            eliminar_tarea(gestion)
+        elif opcion == '6':
+            mostrar_tareas(gestion)
+        elif opcion == '7':
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción no válida. Por favor elija una opción válida.")
+
